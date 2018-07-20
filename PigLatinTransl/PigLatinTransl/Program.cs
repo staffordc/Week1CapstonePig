cbf165a08add6f7fff18baa32bd5ffcc139577f4
staffordc <stafford.carh@gmail.com>
2018-07-20 16:39:36 -0400

    vowel find done

--- PigLatinTransl/PigLatinTransl/Program.cs
+++ PigLatinTransl/PigLatinTransl/Program.cs
@@ -8,8 +8,49 @@ namespace PigLatinTransl
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            int positionOfVowel = -1;
+
+            Console.WriteLine("Welcome to the Pig Latin translator:");
+            Console.WriteLine("Enter-way ext-tay");
+            // input for PLTranslations
+            string userInput = Console.ReadLine();
+
+            StringBuilder pigLatinPrefix = new StringBuilder();
+
+            for (int n = 0; n < userInput.Length; n++)
+            {
+                //int findVowel();
+                //Console.WriteLine($"{PLRoot}{PLPrefix}");
+                //userInput.Substring(startIndex: n);
+                
+                switch (userInput[n])
+                {
+
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        string PLRoot = userInput.Substring(n, userInput.Length);
+                        return;
+
+                    default:
+                        string PLPrefix = userInput.Substring(userInput.Length, n);
+                        return;
+                }
+
+            }
+            Console.ReadKey();
+            if (positionOfVowel == -1)
+            {
+                Console.WriteLine(userInput);
+            }
         }
+                   
     }
 }
+
